79ee3d58f413f073384b1e5aec5a3b3a42fa6160
Tahsin Yazkan <tahsin@tahsins-mbp.bilgi.edu.tr>
2018-04-30 10:31:47 +0300

    Initial Commit

---
+++ Set/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Tahsin Yazkan on 30.04.2018.
+//  Copyright © 2018 Tahsin Yazkan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
